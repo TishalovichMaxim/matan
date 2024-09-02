eb445443b4fe30c01215c02a399fa78eb53a6a4e
Tishalovich Maxim <tishalovichm@gmail.com>
2024-09-02 19:38:48 +0300

    Fix errors

--- math_functions.py
+++ math_functions.py
@@ -1,27 +1,38 @@
 def get_mean(data):
-    return sum(map(lambda i : i[0], data))/len(data)
+    return sum(data)/len(data)
 
-def get_fashion(data):
-    res = 0
-    for i in range(1, len(data)):
-        if (data[i][1] > data[res][1]): 
-            res = i
+def calc_freq_table(data):
+    res = {}
+    for x in data:
+        if not x in res:
+            res[x] = 1
+        else:
+            res[x] += 1
 
     return res
+
+def get_fashion(freq_table):
+    res_x = None
+    res_x_freq = -1
+    for x in freq_table:
+        if freq_table[x] > res_x_freq:
+            res_x = x
+
+    return res_x
 
 def get_mediana(data):
     n = len(data)
     if n % 2 == 0:
         l = n//2 - 1
         r = n//2
-        return (data[l][1] + data[r][1])/2
+        return (data[l] + data[r])/2
     else:
-        return data[n//2][1]
+        return data[n//2]
 
 def get_variance(data):
     mean = get_mean(data)
 
-    return sum(map(lambda p: (p[1] - mean)**2, data))/len(data)
+    return sum(map(lambda x: (x - mean)**2, data))/len(data)
 
 def get_coeff_of_variation(data):
     return get_variance(data)/get_mean(data)
