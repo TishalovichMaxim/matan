e84b42c7b5932c22e51f7934e5003b8c843f6d62
Tishalovich Maxim <tishalovichm@gmail.com>
2024-09-02 11:52:49 +0300

    Add mediana

--- math_functions.py
+++ math_functions.py
@@ -9,3 +9,12 @@ def get_fashion(data):
 
     return res
 
+def get_mediana(data):
+    n = len(data)
+    if n % 2 == 0:
+        l = n//2 - 1
+        r = n//2
+        return (data[l][1] + data[r][1])/2
+    else:
+        return data[n//2][1]
+
